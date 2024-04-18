6a1731fe4898bad3a2bccac2098ff328cbe73c0c
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-04-18 13:29:00 +0200

    werknemer data added

---
+++ Models/Werknemer.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoPeriode8.Models
+{
+    public class Werknemer
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Email { get; set; }
+        public string Telefoonnummer { get; set; }
+        public string Postcode { get; set; }
+        public string Straatnaam { get; set; }
+        public string Stad { get; set; }
+
+    }
+}

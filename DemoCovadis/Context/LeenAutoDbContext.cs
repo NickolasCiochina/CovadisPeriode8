b7a0789c4199f02adcadf9d3f90f4d1f8c9b8116
Nickolas Ciochina <nickolas.ciochina@student.graafschapcollege.nl>
2024-05-29 11:56:56 +0200

    Merge conflict solve

--- DemoCovadis/Context/LeenAutoDbContext.cs
+++ DemoCovadis/Context/LeenAutoDbContext.cs
@@ -6,27 +6,15 @@ namespace DemoCovadis.Context
     public class LeenautoDbContext : DbContext
     {
 
-        public DbSet<Auto> Autos { get; set; }
+        public DbSet<Auto> Auto { get; set; }
         public DbSet<Chauffeur> Chauffeurs { get; set; }
+        public DbSet<Reservering> Reserveringen { get; set; }
         public DbSet<User> Users { get; set; }
-
-        public object User {  get; internal set; }
+        public object User { get; internal set; }
 
         public LeenautoDbContext(DbContextOptions<LeenautoDbContext> options)
            : base(options)
         {
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>().HasData(
-                new User
-                {
-                    Id = 2,
-                    Name = "User",
-                    Email = "user@example.com",
-                    Password = "UserPassword"
-                });
         }
     }
 }

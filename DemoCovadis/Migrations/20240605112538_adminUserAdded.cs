30399a3cabfb78f156d7fcb43c2d50c5fd8556b2
Nickolas Ciochina <nickolas.ciochina@student.graafschapcollege.nl>
2024-06-21 21:15:44 +0200

    admin user duplicate solve

--- DemoCovadis/Migrations/20240605112538_adminUserAdded.cs
+++ DemoCovadis/Migrations/20240605112538_adminUserAdded.cs
@@ -1,28 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿//using Microsoft.EntityFrameworkCore.Migrations;
 
-#nullable disable
+//#nullable disable
 
-namespace DemoCovadis.Migrations
-{
-    /// <inheritdoc />
-    public partial class adminUserAdded : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.InsertData(
-                table: "User",
-                columns: new[] { "Id", "Email", "Name", "Password" },
-                values: new object[] { 1, "admin@example.com", "Admin", "AdminPassword" });
-        }
+//namespace DemoCovadis.Migrations
+//{
+//    /// <inheritdoc />
+//    public partial class adminUserAdded : Migration
+//    {
+//        /// <inheritdoc />
+//        protected override void Up(MigrationBuilder migrationBuilder)
+//        {
+//            migrationBuilder.InsertData(
+//                table: "User",
+//                columns: new[] { "Id", "Email", "Name", "Password" },
+//                values: new object[] { 1, "admin@example.com", "Admin", "AdminPassword" });
+//        }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DeleteData(
-                table: "User",
-                keyColumn: "Id",
-                keyValue: 1);
-        }
-    }
-}
+//        /// <inheritdoc />
+//        protected override void Down(MigrationBuilder migrationBuilder)
+//        {
+//            migrationBuilder.DeleteData(
+//                table: "User",
+//                keyColumn: "Id",
+//                keyValue: 1);
+//        }
+//    }
+//}
